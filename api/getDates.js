f300180d8539efa57b2e05811b34b74ee4ef78a8
M Dwi Saputra <96591078+mdsaputra@users.noreply.github.com>
2025-10-06 22:43:16 +0700

    Update getDates.js

--- api/getDates.js
+++ api/getDates.js
@@ -1,6 +1,14 @@
-// api/getDates.js
-import { getSavedDates } from "./saveDate.js";
+import fs from "fs";
+import path from "path";
 
 export default async function handler(req, res) {
-  res.status(200).json(getSavedDates());
+  const filePath = path.join(process.cwd(), "data.json");
+
+  try {
+    const file = fs.readFileSync(filePath, "utf8");
+    const data = JSON.parse(file);
+    res.status(200).json(data);
+  } catch {
+    res.status(200).json([]);
+  }
 }
