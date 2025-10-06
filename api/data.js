d11c0139a659c85557fb549e85a579c954030210
M Dwi Saputra <96591078+mdsaputra@users.noreply.github.com>
2025-10-06 22:30:19 +0700

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,1 +1,12 @@
+export default async function handler(req, res) {
+  const fs = require("fs");
+  const path = "./data.json";
+
+  if (fs.existsSync(path)) {
+    const data = JSON.parse(fs.readFileSync(path, "utf-8"));
+    return res.status(200).json(data);
+  } else {
+    return res.status(200).json([]);
+  }
+}
 
