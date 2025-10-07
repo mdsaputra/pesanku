78abaa9bbdb3db1c60efe17f1241b622b2ce7ab2
M Dwi Saputra <96591078+mdsaputra@users.noreply.github.com>
2025-10-07 16:55:29 +0700

    Update getDates.js

--- api/getDates.js
+++ api/getDates.js
@@ -1,6 +1,6 @@
 import { list } from "@vercel/blob";
 
-export const config = { runtime: "edge" };
+export const config = { runtime: "nodejs" };
 
 export default async function handler() {
   try {
